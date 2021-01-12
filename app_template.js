cabe6b2c295079db1db0f1df4339e1567a1c7cee
Erzengel792 <tanadol_boat@hotmail.com>
2021-01-12 13:57:37 +0700

    edited v.1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,6 @@
 //your code here!
+function studentID(){
+    var x = document.getElementById("input-number").value;
+    document.getElementById("input-number").innerHTML = x;
+    console . log("x");
+}
